8ae72b8fa6c71fd81099d9601aeae38ad9c6e8bc
WS8665-epics <ws8665-epics@WS8665.zit.bam.de>
2024-12-16 17:50:16 +0100

    seems to work, in testing

--- dectris_eiger_ioc.py
+++ dectris_eiger_ioc.py
@@ -11,6 +11,7 @@ import os
 import asyncio
 from validators import validate_ip_address, validate_port_number, ensure_directory_exists_and_is_writeable
 from custom_operations import CustomPostExposureOperation
+import time
 
 import logging
 
@@ -56,10 +57,14 @@ class DEigerIOC(PVGroup):
     LocalFileDumpPath: Path = attrs.field(default=Path("/tmp"), validator=[attrs.validators.instance_of(Path), ensure_directory_exists_and_is_writeable])
     # number of frames to be taken in a single exposure
     _nframes: int = attrs.field(default=1, validator=attrs.validators.optional(attrs.validators.instance_of(int)))
+    _nimages_per_file:int = attrs.field(default=1800, validator=attrs.validators.instance_of(int))
     # start time of the exposure
     _starttime: datetime = attrs.field(default=None, validator=attrs.validators.optional(attrs.validators.instance_of(datetime)))
     # for any location-specific operations that need to be performed after data collection
     custom_post_exposure_operation: CustomPostExposureOperation = attrs.field(factory=CustomPostExposureOperation)
+    # if we tried writing while the detector was initializing or measuring:
+    _parameters_not_applied:bool = attrs.field(default=False, validator=attrs.validators.instance_of(bool))
+    _detector_initialized:bool = attrs.field(default=False, validator=attrs.validators.instance_of(bool))
 
     def __init__(self, *args, **kwargs) -> None:
         for k in list(kwargs.keys()):
@@ -69,6 +74,9 @@ class DEigerIOC(PVGroup):
         self.client = DEigerClient(self.host, port=self.port)
         super().__init__(*args, **kwargs)
         self._starttime = None #datetime.now(timezone.utc)
+        self._nimages_per_file = 1800
+        self._detector_initialized = False
+        # self._parameters_not_applied = False
 
     def empty_data_store(self):
         self.client.sendFileWriterCommand("clear")
@@ -78,7 +86,18 @@ class DEigerIOC(PVGroup):
     def restart_and_initialize_detector(self):
         logging.info("restarting detector")        
         self.client.sendSystemCommand("restart")
+        time.sleep(.1)
         self.client.sendDetectorCommand("initialize")
+        ntry = 5
+        while self.DetectorState.value in ['na', 'error']:
+            print(f'failure to initialize detector, trying again {ntry} times out of 5')
+            time.sleep(.1)
+            self.client.sendDetectorCommand("initialize")
+            ntry -=1
+            if ntry <0:
+                print(f'FAILURE TO INITIALIZE detector')
+        self._detector_initialized = True
+
 
     def set_energy_values(self, PhotonEnergy = None, ThresholdEnergy = None):
         if PhotonEnergy is None:
@@ -108,7 +127,7 @@ class DEigerIOC(PVGroup):
     def set_filewriter_config(self):
         self.client.setFileWriterConfig("mode", "enabled") # write HDF5 files
         self.client.setFileWriterConfig("name_pattern", f"{self.OutputFilePrefix.value}$id")
-        self.client.setFileWriterConfig("nimages_per_file", 1800) # maximum 1800 frames per file
+        self.client.setFileWriterConfig("nimages_per_file", self._nimages_per_file) # maximum 1800 frames per file
         self.client.fileWriterConfig("compression_enabled")
         self.client.setDetectorConfig("compression", "bslz4")
     
@@ -122,6 +141,10 @@ class DEigerIOC(PVGroup):
     def configure_detector(self):     
         """ runs all the required detector initializations before a measurement"""   
         # self.restart_and_initialize_detector() # not sure this is necessary every time
+
+        if not self._detector_initialized:
+            print('before configuring the detector, I will intialize it at least once...')
+            self.restart_and_initialize_detector()
         self.set_energy_values()
         self.set_timing_values()
         self.empty_data_store()
@@ -134,7 +157,6 @@ class DEigerIOC(PVGroup):
         """ reads the detector configuration of a particular key and returns it as a dictionary. Safely handles errors"""
         try:
             answer = self.client.detectorStatus(key)
-            print(answer)
             if not isinstance(answer, dict):
                 return default
             else:
@@ -144,11 +166,25 @@ class DEigerIOC(PVGroup):
 
     def read_and_dump_files(self):
         """ reads all files in the data store and dumps them to disk at the location specified upon IOC init"""
+
+        expected_number_of_files = np.ceil(self._nframes/self._nimages_per_file)+1
         # TODO: check how this writes files during measurememnt - does it update as files are created, or does it create files only on completion?
+        
         filenames = self.client.fileWriterFiles()#['value'] # returns all files in datastore
+        ntry = 200
+        while not len(filenames)>=expected_number_of_files:
+            time.sleep(.1)
+            filenames = self.client.fileWriterFiles()#['value'] # returns all files in datastore
+            if ntry <0:
+                print('did not find the needed number of files after 20 seconds')
+                return 
+            ntry -= 1
+
+        print(f'filenames found: {filenames}')
         for filename in filenames:
             if filename in os.listdir(self.LocalFileDumpPath) or not filename.startswith(self.OutputFilePrefix.value):
                 continue # skip if file already exists or is one we're not looking for
+            print(f'retrieving: {filename}')
             self.client.fileWriterSave(filename, self.LocalFileDumpPath)
             self.LatestFile=str(filename)
             if 'master' in filename:
@@ -163,46 +199,73 @@ class DEigerIOC(PVGroup):
 
     def ok_to_set_parameters(self)->bool:
         if self.DetectorState.value != 'idle':
-            print('cannot set parameters if detector is not idle')
+            print('CANNOT SET PARAMETERS if detector is not idle')
+            self._parameters_not_applied=True
             return False
-        if self.Initialize_RBV.value is not False:
-            print('cannot set parameters if initalization is busy')
+            
+        if self.Initialize_RBV.value not in ['Off', False]:
+            print('CANNOT SET PARAMETERS if initalization is busy')
+            self._parameters_not_applied=True
             return False
+
         return True
 
+    async def ensure_parameters_applied(self):
+        if self._parameters_not_applied:
+            loop = asyncio.get_event_loop()
+            await loop.run_in_executor(None, self.configure_detector)
+
+    async def check_ready_to_trigger(self)->bool:
+        await ensure_parameters_applied()
+            
+        if not self.ok_to_set_parameters():
+            return False
+        if not _detector_initialized:
+            return False
+        await self.ReadyToTrigger.write(True)
+
+    async def wait_for_init_complete(self):
+        counter = 0
+        if self.DetectorState.value in ['na', 'error']:
+            print('error state detected in detector, reinitializing before triggering...')
+            await self.Initialize.write(True)
+            await asyncio.sleep(.1)
+            
+        while self.Initialize_RBV.value not in ['Off', False]:
+            counter += 1
+            await asyncio.sleep(.1)
+            if (counter % 10 == 0):
+                print('waiting for initialization to complete...')
 
     # Detector state readouts
-    DetectorState = pvproperty(doc="State of the detector, can be 'busy' or 'idle'", dtype=str, record='stringin',
+    DetectorState = pvproperty(value = '', doc="State of the detector, can be 'busy' or 'idle'", dtype=str, record='stringin',
                                report_as_string=True)
-    DetectorTemperature = pvproperty(doc="Temperature of the detector", dtype=float, record='ai')
-    DetectorTime = pvproperty(doc="Timestamp on the detector", dtype=str, record='stringin', report_as_string=True)
-    CountTime_RBV = pvproperty(doc="Gets the actual total exposure time the detector", dtype=float, record='ai')
-    FrameTime_RBV = pvproperty(doc="Gets the actual frame time from the detector", dtype=float, record='ai')
+    DetectorTemperature = pvproperty(value = -999.9, doc="Temperature of the detector", dtype=float, record='ai')
+    DetectorTime = pvproperty(value = '', doc="Timestamp on the detector", dtype=str, record='stringin', report_as_string=True)
+    CountTime_RBV = pvproperty(value = 600.0, doc="Gets the actual total exposure time the detector", dtype=float, record='ai')
+    FrameTime_RBV = pvproperty(value = 10.0, doc="Gets the actual frame time from the detector", dtype=float, record='ai')
 
     # settables for the detector
-    ThresholdEnergy = pvproperty(value = 4025, doc="Sets the energy threshold on the detector, normally 0.5 * PhotonEnergy", dtype=float, record='ao')
-    PhotonEnergy = pvproperty(value = 8050, doc="Sets the photon energy on the detector", dtype=int, record='ai')
-    FrameTime = pvproperty(doc="Sets the frame time on the detector. nominally should be <= CountTime", value=10.0, record='ai')
-    CountTime = pvproperty(doc="Sets the total exposure time the detector", value=600.0, record='ai')
-    CountRateCorrection = pvproperty(doc="do you want count rate correction applied by the detector (using int maths)", value=True , record='bi')
-    FlatFieldCorrection = pvproperty(doc="do you want flat field correction applied by the detector (using int maths)", value=False, record='bi')
-    PixelMaskCorrection = pvproperty(doc="do you want pixel mask correction applied by the detector", value=False, record='bi')
+    ThresholdEnergy = pvproperty(value = 4025.0, doc="Sets the energy threshold on the detector, normally 0.5 * PhotonEnergy", record='ao')
+    PhotonEnergy = pvproperty(value = 8050.0, doc="Sets the photon energy on the detector", record='ai')
+    FrameTime = pvproperty(value = 10.0, doc="Sets the frame time on the detector. nominally should be <= CountTime", record='ai')
+    CountTime = pvproperty(value = 600.0, doc="Sets the total exposure time the detector", record='ai')
+    CountRateCorrection = pvproperty(value = True, doc="do you want count rate correction applied by the detector (using int maths)", record='bi')
+    FlatFieldCorrection = pvproperty(value = False, doc="do you want flat field correction applied by the detector (using int maths)", record='bi')
+    PixelMaskCorrection = pvproperty(value = False, doc="do you want pixel mask correction applied by the detector", record='bi')
 
     # operating the detector
-    Initialize = pvproperty(doc="Initialize the detector, resets to False immediately", dtype=bool, record='bo')
-    Initialize_RBV = pvproperty(doc="True while detector is initializing", dtype=bool, record='bo')
-    Trigger = pvproperty(doc="Trigger the detector to take an image, resets to False immediately. Adjusts detector_state to 'busy' for the duration of the measurement.", value=False, record='bi')
-    Trigger_RBV = pvproperty(doc="True while the detector capture subroutine in the IOC is busy", dtype=bool, record='bo')
+    Initialize = pvproperty(value = False, doc="Initialize the detector, resets to False immediately", dtype=bool, record='bo')
+    Initialize_RBV = pvproperty(value = False, doc="True while detector is initializing", dtype=bool, record='bo')
+    Trigger = pvproperty(value = False, doc="Trigger the detector to take an image, resets to False immediately. Adjusts detector_state to 'busy' for the duration of the measurement.", record='bi')
+    Trigger_RBV = pvproperty(value = False, doc="True while the detector capture subroutine in the IOC is busy", dtype=bool, record='bo')
     OutputFilePrefix = pvproperty(value="eiger_", doc="Set the prefix of the main and data output files", dtype=str, record='stringin', report_as_string=True)
-    LatestFile = pvproperty(doc="Shows the name of the latest output file retrieved", dtype=str, record='stringin', report_as_string=True)
-    LatestFileData = pvproperty(doc="Shows the name of the latest output data file retrieved", dtype=str, record='stringin', report_as_string=True)
-    LatestFileMain = pvproperty(doc="Shows the name of the latest output main file retrieved", dtype=str, record='stringin', report_as_string=True)
-    SecondsRemaining = pvproperty(doc="Shows the seconds remaining for the current exposure", dtype=int, record='longin')
-    FileScanner = pvproperty(doc="Scans the data store for new files and dumps them to disk", dtype=bool, record='bi')
+    LatestFile = pvproperty(value = '', doc="Shows the name of the latest output file retrieved", dtype=str, record='stringin', report_as_string=True)
+    LatestFileData = pvproperty(value = '', doc="Shows the name of the latest output data file retrieved", dtype=str, record='stringin', report_as_string=True)
+    LatestFileMain = pvproperty(value = '', doc="Shows the name of the latest output main file retrieved", dtype=str, record='stringin', report_as_string=True)
+    SecondsRemaining = pvproperty(value = 0, doc="Shows the seconds remaining for the current exposure", dtype=int, record='longin')
+    ReadyToTrigger = pvproperty(value = False, doc="Set to False when not initialized, when setting parameters, etc. only True when truly ready. Remember to Initialize before Trigger", record='bi')
 
-    # @FileScanner.scan(period=5, use_scan_field=True, subtract_elapsed=True)
-    # async def FileScanner(self, instance, async_lib):
-    #     self.read_and_dump_files()
 
     @DetectorState.scan(period=5, use_scan_field=True, subtract_elapsed=True)
     async def DetectorState(self, instance, async_lib):
@@ -231,19 +294,31 @@ class DEigerIOC(PVGroup):
 
     @ThresholdEnergy.putter
     async def ThresholdEnergy(self, instance, value: float):
-        self.set_energy_values(ThresholdEnergy = value)
+        await self.ReadyToTrigger.write(False)
+        if self.ok_to_set_parameters():
+            self.set_energy_values(ThresholdEnergy = value)
+        await self.check_ready_to_trigger()
 
     @PhotonEnergy.putter
-    async def PhotonEnergy(self, instance, value: int):
-        self.set_energy_values(PhotonEnergy = Value)
+    async def PhotonEnergy(self, instance, value: float):
+        await self.ReadyToTrigger.write(False)
+        if self.ok_to_set_parameters():
+            self.set_energy_values(PhotonEnergy = value)
+        await self.check_ready_to_trigger()
 
     @FrameTime.putter
     async def FrameTime(self, instance, value: float):
-        self.set_timing_values(FrameTime = value)
+        await self.ReadyToTrigger.write(False)
+        if self.ok_to_set_parameters():
+            self.set_timing_values(FrameTime = value)
+        await self.check_ready_to_trigger()
 
     @CountTime.putter
     async def CountTime(self, instance, value: float):
-        self.set_timing_values(CountTime = value)    
+        await self.ReadyToTrigger.write(False)
+        if self.ok_to_set_parameters():
+            self.set_timing_values(CountTime = value)    
+        await self.check_ready_to_trigger()
 
     @CountTime.getter
     async def CountTime(self, instance):
@@ -251,6 +326,7 @@ class DEigerIOC(PVGroup):
 
     @Initialize.putter
     async def Initialize(self, instance, value: bool):
+        await self.ReadyToTrigger.write(False)
         if value:
             await self.Initialize_RBV.write(True)
             loop = asyncio.get_event_loop()
@@ -258,33 +334,24 @@ class DEigerIOC(PVGroup):
             await loop.run_in_executor(None, self.configure_detector)
             value=False
             await self.Initialize_RBV.write(False)
+        await self.check_ready_to_trigger()
         # await self.Initialize.write(False)
-
-    async def wait_for_init_complete(self):
-        counter = 0
-        print(self.Initialize_RBV.value)
-        while self.Initialize_RBV.value is not 'Off':
-            counter += 1
-            await asyncio.sleep(.1)
-            if (counter % 10 == 0):
-                print('waiting for initialization to complete...')
 
     @Trigger.putter
     async def Trigger(self, instance, value: bool):
         if value:
+            await self.Trigger_RBV.write(True)
+            await self.ReadyToTrigger.write(False)
             # ensure initialisation is complete first..
             await self.wait_for_init_complete()
-            await self.Trigger_RBV.write(True)
-
-            # await self.Trigger.write(False)
-            self._starttime = datetime.now(timezone.utc)
-
             loop = asyncio.get_event_loop()
             await loop.run_in_executor(None, self.client.sendDetectorCommand, "arm")
+            self._starttime = datetime.now(timezone.utc)
             await loop.run_in_executor(None, self.client.sendDetectorCommand, "trigger") # can this be done with await? it's not an async function...
             await loop.run_in_executor(None, self.client.sendDetectorCommand, "disarm")
             await loop.run_in_executor(None, self.retrieve_all_and_clear_files)
             await self.Trigger_RBV.write(False)
+        await self.check_ready_to_trigger()
             
 def main(args=None):
     parser, split_args = template_arg_parser(
